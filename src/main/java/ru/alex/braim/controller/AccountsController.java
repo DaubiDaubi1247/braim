9da5ab30d9ba1c35a304d6bbe1ec2427a1956e0f
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-02-07 15:31:54 +0300

    create AccountsController

---
+++ src/main/java/ru/alex/braim/controller/AccountsController.java
@@ -0,0 +1,21 @@
+package ru.alex.braim.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accounts")
+@RequiredArgsConstructor
+public class AccountsController {
+
+    private final AccountService accountService;
+
+    @GetMapping("/{accountId}")
+    public ResponseEntity<AccountDto> getAccountInfo(@PathVariable Long accountId) {
+        return AccountService
+    }
+}
