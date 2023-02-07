c62289c244da874306e8915d11ee762443546fc4
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-02-07 15:27:30 +0300

    start commit

---
+++ src/main/java/ru/alex/braim/BraimApplication.java
@@ -0,0 +1,13 @@
+package ru.alex.braim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BraimApplication.class, args);
+    }
+
+}
