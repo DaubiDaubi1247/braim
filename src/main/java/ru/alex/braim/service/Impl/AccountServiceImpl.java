38d3980e3d2a571ed2483c225c3cc2f05a6e1b73
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-02-09 20:01:06 +0300

    create getAccountsByParameters service method

--- src/main/java/ru/alex/braim/service/Impl/AccountServiceImpl.java
+++ src/main/java/ru/alex/braim/service/Impl/AccountServiceImpl.java
@@ -2,14 +2,17 @@ package ru.alex.braim.service.Impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.alex.braim.dto.AccountDto;
 import ru.alex.braim.entity.Account;
 import ru.alex.braim.exception.NotFoundException;
 import ru.alex.braim.mapper.AccountMapper;
 import ru.alex.braim.repository.AccountRepository;
 import ru.alex.braim.service.AccountService;
+import ru.alex.braim.specification.AccountSpecification;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,13 +20,22 @@ public class AccountServiceImpl implements AccountService {
     private final AccountRepository accountRepository;
     private final AccountMapper accountMapper;
     @Override
+    @Transactional
     public AccountDto getAccountById(Long id) {
         return accountMapper.toDto(getAccountEntityById(id));
     }
 
     @Override
+    @Transactional
     public List<AccountDto> getAccountsByParameters(AccountDto accountDto, Integer from, Integer size) {
-        return null;
+        List<Account> accountList = accountRepository.findAll(AccountSpecification.
+                getAccountSpecificationByParameters(accountDto));
+
+        return accountList.stream()
+                .skip(from)
+                .limit(size)
+                .map(accountMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     private Account getAccountEntityById(Long id) {
