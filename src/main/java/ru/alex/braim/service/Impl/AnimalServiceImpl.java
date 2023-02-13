f9ebfde2fc363f2baec9dbef11a59ff6cd60ad55
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-02-13 14:15:29 +0300

    add AnimalRepository into AnimalServiceImpl

--- src/main/java/ru/alex/braim/service/Impl/AnimalServiceImpl.java
+++ src/main/java/ru/alex/braim/service/Impl/AnimalServiceImpl.java
@@ -1,9 +1,16 @@
 package ru.alex.braim.service.Impl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.alex.braim.dto.AnimalResponseDto;
+import ru.alex.braim.repository.AnimalRepository;
 import ru.alex.braim.service.AnimalService;
 
+@Service
+@RequiredArgsConstructor
 public class AnimalServiceImpl implements AnimalService {
+
+    private final AnimalRepository animalRepository;
 
     @Override
     public AnimalResponseDto getAnimalById(Long id) {
