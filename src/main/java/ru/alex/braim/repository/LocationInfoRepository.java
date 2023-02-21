5c5e47feff26d48a4692f54cbcd3275386a3b827
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-02-21 20:08:47 +0300

    debug LocationInfoRepository

--- src/main/java/ru/alex/braim/repository/LocationInfoRepository.java
+++ src/main/java/ru/alex/braim/repository/LocationInfoRepository.java
@@ -3,6 +3,7 @@ package ru.alex.braim.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.alex.braim.dto.LocationProjection;
 import ru.alex.braim.entity.LocationInfo;
 
@@ -17,6 +18,9 @@ public interface LocationInfoRepository extends JpaRepository<LocationInfo, Long
             "JOIN a.chippingInfoList ci " +
             "JOIN a.locationList li " +
             "WHERE a.id = :animalId " +
-            "   AND ci.chippingDateTime BETWEEN :startDate AND :endDate")
-    List<LocationProjection> findLocationPointByParams(Date startDate, Date endDate, Long animalId);
+            "   AND (CAST(:startDate AS timestamp)  IS null OR ci.chippingDateTime >= :startDate) " +
+            "   AND (CAST(:endDate AS timestamp) IS null OR :endDate >= ci.chippingDateTime) ")
+    List<LocationProjection> findLocationPointByParams(@Param("startDate") Date startDate,
+                                                       @Param("endDate") Date endDate,
+                                                       @Param("animalId") Long animalId);
 }
