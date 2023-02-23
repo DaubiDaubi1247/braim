788ab7ec1b3891146dc5547ce85403983a7c8531
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-02-23 11:31:31 +0300

    add password in AccountDto, and set @NotBlank annotation

--- src/main/java/ru/alex/braim/dto/AccountDto.java
+++ src/main/java/ru/alex/braim/dto/AccountDto.java
@@ -1,6 +1,7 @@
 package ru.alex.braim.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +14,15 @@ import lombok.NoArgsConstructor;
 public class AccountDto {
     private Long id;
 
+    @NotBlank(message = "firstName cant be empty")
     private String firstName;
 
+    @NotBlank(message = "lastName cant be empty")
     private String lastName;
 
-    @Email(message = "почта не соотвествует шаблону")
+    @Email(message = "email doesnt matches pattern")
     private String email;
+
+    @NotBlank(message = "password cant be empty")
+    private String password;
 }
