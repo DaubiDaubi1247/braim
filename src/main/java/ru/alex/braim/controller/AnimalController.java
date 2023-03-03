d2a836253c8a3470634d292fa6982b39b63d19c9
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-03-03 15:59:17 +0300

    create createAnimal endpoint in AnimalController

--- src/main/java/ru/alex/braim/controller/AnimalController.java
+++ src/main/java/ru/alex/braim/controller/AnimalController.java
@@ -1,11 +1,10 @@
 package ru.alex.braim.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.alex.braim.dto.AnimalDto;
 import ru.alex.braim.dto.AnimalProjection;
 import ru.alex.braim.dto.LocationProjection;
 import ru.alex.braim.requestParam.AnimalRequestParams;
@@ -22,6 +21,13 @@ public class AnimalController {
 
     private final AnimalService animalService;
     private final LocationService locationService;
+
+    @PostMapping
+    public ResponseEntity<AnimalProjection> createAnimal(@RequestBody AnimalDto animalDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(animalService.createAnimal(animalDto));
+    }
+
+
 
     @GetMapping("/{animalId}")
     public ResponseEntity<AnimalProjection> getAnimalById(@PathVariable Long animalId) {
